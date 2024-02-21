ccf3af6fb26204d98d89e8d72cbbfe3c20a825ef
Inigo Merckx <inigo@easics.be>
2024-02-21 23:25:03 +0100

    Added the binary search code example

--- templates/code_examples/binary_search.py
+++ templates/code_examples/binary_search.py
@@ -1,3 +1,64 @@
 
-def binary_search():
-  TODO
+def binary_search(values, elem):
+
+  low = 0
+  high = len(values) - 1
+  mid = 0
+
+  while low <= high:
+
+    mid = (high + low) // 2
+
+    if values[mid] < elem:
+      low = mid + 1
+
+    elif values[mid] > elem:
+      high = mid - 1
+
+    else:
+      return mid
+
+  # If we reach here, then the element was not present
+  return -1
+
+def binary_search_intert_right():
+
+  values = [2,5,6,8,9,14]
+
+  low = 0
+  high = len(values)
+  mid = 0
+
+  x = 6
+
+  while low < high:
+
+    mid = (low + high) // 2
+
+    if x < values[mid]:
+      high = mid
+    else:
+      low = mid + 1
+
+  values.insert(low, x)
+
+def binary_search_intert_left():
+
+  values = [2,5,6,8,9,14]
+
+  low = 0
+  high = len(values)
+  mid = 0
+
+  x = 5
+
+  while low < high:
+
+    mid = (low + high) // 2
+
+    if x > values[mid]:
+      low = mid + 1
+    else:
+      high = mid
+
+  values.insert(low, x)
