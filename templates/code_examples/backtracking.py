c332c0d989d3c0ef65c2cce69dc7631e26403e81
Inigo Merckx <inigo@easics.be>
2024-03-05 01:34:11 +0100

    Added code example for backtracking

--- templates/code_examples/backtracking.py
+++ templates/code_examples/backtracking.py
@@ -1,3 +1,46 @@
 
 def backtracking():
-  TODO
+
+  connections = {1 : [1, 2, 3],
+                 2 : [2, 3],
+                 3 : [3, 1]}
+
+  max_moves = 0
+  curr_moves = 0
+
+  to_visit = set(connections.keys())
+
+  def solve(occupied, visited, to_visit):
+    nonlocal max_moves
+    nonlocal curr_moves
+
+    # Can be replaced by a branch cutting condition for extra speed up.
+    # For example:
+    #   if len(to_visit) + curr_moves > max_moves:
+    if True:
+      curr_house = to_visit.pop()
+      visited.add(curr_house)
+
+      copy_moves = curr_moves
+
+      for house in connections[curr_house]:
+        if house not in occupied:
+          occupied.add(house)
+
+          if house != curr_house:
+            curr_moves += 1
+
+          if len(to_visit) > 0:
+            solve(occupied, visited, to_visit)
+          else:
+            max_moves = max(curr_moves, max_moves)
+
+          curr_moves = copy_moves
+          occupied.remove(house)
+
+      visited.remove(curr_house)
+      to_visit.add(curr_house)
+
+  solve(set(), set(), to_visit)
+
+  result = max_moves
