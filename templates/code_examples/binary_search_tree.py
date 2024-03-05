0c1b5e018a57d8cb7022e874ac7b7bf48b233b1a
Inigo Merckx <inigo@easics.be>
2024-03-05 16:41:46 +0100

    Added binary search tree example

---
+++ templates/code_examples/binary_search_tree.py
@@ -0,0 +1,162 @@
+
+class Binary_search_tree_node:
+
+  def __init__(self, val = None):
+    self.left = None
+    self.right = None
+    self.val = val
+
+  def insert(self, val):
+
+    if not self.val:
+      self.val = val
+      return
+
+    # This removes duplicates
+    #if self.val == val:
+    #  return
+
+    if val < self.val:
+      if self.left:
+        self.left.insert(val)
+        return
+
+      self.left = Binary_search_tree_node(val)
+      return
+
+    if self.right:
+      self.right.insert(val)
+      return
+
+    self.right = Binary_search_tree_node(val)
+
+  def get_min(self):
+    current = self
+    while current.left is not None:
+      current = current.left
+    return current.val
+
+  def get_max(self):
+    current = self
+    while current.right is not None:
+      current = current.right
+    return current.val
+
+  # Note: If the node is not in the tree we delete a node close to it
+  def delete(self, val):
+
+    if self == None:
+      return self
+
+    if self.right == None:
+      return self.left
+
+    if self.left == None:
+      return self.right
+
+    if val < self.val:
+      if self.left:
+        self.left = self.left.delete(val)
+      return self
+
+    if val > self.val:
+      if self.right:
+        self.right = self.right.delete(val)
+      return self
+
+    min_larger_node = self.right
+    while min_larger_node.left:
+      min_larger_node = min_larger_node.left
+
+    self.val = min_larger_node.val
+    self.right = self.right.delete(min_larger_node.val)
+
+    return self
+
+  def exists(self, val):
+
+    if val == self.val:
+      return True
+
+    if val < self.val:
+      if self.left == None:
+        return False
+      return self.left.exists(val)
+
+    if self.right == None:
+      return False
+
+    return self.right.exists(val)
+
+  def preorder(self, vals):
+
+    if self.val is not None:
+      vals.append(self.val)
+
+    if self.left is not None:
+      self.left.preorder(vals)
+
+    if self.right is not None:
+      self.right.preorder(vals)
+
+    return vals
+
+  def inorder(self, vals):
+
+    if self.left is not None:
+      self.left.inorder(vals)
+
+    if self.val is not None:
+      vals.append(self.val)
+
+    if self.right is not None:
+      self.right.inorder(vals)
+
+    return vals
+
+  def postorder(self, vals):
+
+    if self.left is not None:
+      self.left.postorder(vals)
+
+    if self.right is not None:
+      self.right.postorder(vals)
+
+    if self.val is not None:
+      vals.append(self.val)
+
+    return vals
+
+# Driver Code
+if __name__ == '__main__':
+
+  values = [12, 6, 18, 19, 21, 11, 3, 5, 4, 24, 18]
+
+  bst = Binary_search_tree_node()
+
+  for value in values:
+    bst.insert(value)
+
+  print("preorder traversal:")
+  print(bst.preorder([]))
+  print("#")
+
+  print("postorder traversal:")
+  print(bst.postorder([]))
+  print("#")
+
+  print("inorder traversal:")
+  print(bst.inorder([]))
+  print("#")
+
+  delete_values = [2, 6, 20]
+
+  print(f"deleting: {delete_values}")
+  for value in delete_values:
+    bst.delete(value)
+  print("#")
+
+  print(f"4 exists: {bst.exists(4)}")
+  print(f"2 exists: {bst.exists(2)}")
+  print(f"12 exists: {bst.exists(12)}")
+  print(f"18 exists: {bst.exists(18)}")
