5c68ebf48f5c07c89c1b5af7604f5f05fb34eec4
Fromhell777 <inigo.merckx@gmail.com>
2024-03-09 16:59:00 +0100

    Updated the shortes path algorithms to include the shortest path

--- code_examples/shortest_path.py
+++ code_examples/shortest_path.py
@@ -26,6 +26,9 @@ def shortest_path_priority_queue():
   dist = {k : init_large_value for k in graph.keys()}
   dist[start_node] = 0
 
+  prev = {k : None for k in graph.keys()}
+  prev[start_node] = start_node
+
   visited = set()
   q = PriorityQueue()
   q.put((0, start_node))
@@ -42,12 +45,30 @@ def shortest_path_priority_queue():
     if node not in visited:
       visited.add(node)
 
-      for connected_node in nodes[node]:
+      for connected_node in graph[node]:
         if connected_node not in visited:
           extra_cost = costs[(node, connected_node)]
           if dist[connected_node] > dist[node] + extra_cost:
             dist[connected_node] = dist[node] + extra_cost
+            prev[connected_node] = node
             q.put((dist[connected_node], connected_node))
+
+  if prev[end_node] == None:
+    shortest_path = []
+  else:
+    shortest_path = [end_node]
+    while start_node != end_node:
+      end_node = prev[end_node]
+      shortest_path.append(end_node)
+
+    shortest_path.reverse()
+
+  if shortest_dist == init_large_value:
+    print(f"Shortest dist: not reachable")
+  else:
+    print(f"Shortest dist: {shortest_dist}")
+
+  print(f"Shortest path: {shortest_path}")
 
 def shortest_path_heap_queue():
 
@@ -74,6 +95,9 @@ def shortest_path_heap_queue():
   dist = {k : init_large_value for k in graph.keys()}
   dist[start_node] = 0
 
+  prev = {k : None for k in graph.keys()}
+  prev[start_node] = start_node
+
   visited = set()
   queue = []
   heapq.heappush(queue, (0, start_node))
@@ -90,9 +114,27 @@ def shortest_path_heap_queue():
     if node not in visited:
       visited.add(node)
 
-      for connected_node in nodes[node]:
+      for connected_node in graph[node]:
         if connected_node not in visited:
           extra_cost = costs[(node, connected_node)]
           if dist[connected_node] > dist[node] + extra_cost:
             dist[connected_node] = dist[node] + extra_cost
+            prev[connected_node] = node
             heapq.heappush(queue, (dist[connected_node], connected_node))
+
+  if prev[end_node] == None:
+    shortest_path = []
+  else:
+    shortest_path = [end_node]
+    while start_node != end_node:
+      end_node = prev[end_node]
+      shortest_path.append(end_node)
+
+    shortest_path.reverse()
+
+  if shortest_dist == init_large_value:
+    print(f"Shortest dist: not reachable")
+  else:
+    print(f"Shortest dist: {shortest_dist}")
+
+  print(f"Shortest path: {shortest_path}")
